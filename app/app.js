65620d93a933ccf685db2dc9c9b4cf106943500d
Shishir <shishir3324kumar@gmail.com>
2023-12-14 18:07:00 +0530

    Cors installed and added to app.js file. Csv file also installed

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 
 const exerciseRouter = require("./router/exercises");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors())
 
 app.use("/exercises", exerciseRouter);
 
